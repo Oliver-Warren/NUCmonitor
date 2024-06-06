a52c0e18941f8d560af0f31f6060c515a52a024e
Oliver-Warren <aa20575@bristol.ac.uk>
2024-06-06 12:47:39 +0100

    trying to read jsons into df

--- model/model.py
+++ model/model.py
@@ -6,6 +6,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from pathlib import Path
+
+paths = Path("/home/ubuntu/NUCmonitor/data").glob("*.json")
+df = pd.DataFrame([pd.read_json(p, tpy="series") for p in paths])
 
 # Create data set from the jsons output by monitoringtool
 def loadData():

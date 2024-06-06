69a308277f4fd41d52151f7d34ca714a772bd507
Oliver-Warren <aa20575@bristol.ac.uk>
2024-06-06 11:44:55 +0000

    generated some bogus data to test model preprocessing

--- nucMonitor/datagen.py
+++ nucMonitor/datagen.py
@@ -2,14 +2,9 @@ from monitoringTool import *
 import os
 import subprocess
 
-def stressCmd(load=50, cores=3):
-  args = ["stress-ng", "--daemon", "1", "-q", "-c", str(cores), "-l", str(load)]
-  print(" ".join(args))
-  subprocess.run(args)
-
 def gen(testId):
   print("Data generation for test ID:", testId)
-  dirPath = "/home/ubuntu/monitoringTool/test" + str(testId) + "/"
+  dirPath = "/home/ubuntu/NUCmonitor/testData" + str(testId) + "/"
   os.makedirs(os.path.dirname(dirPath), exist_ok=True)
   print("Data points will be stored as json at", dirPath)
   i = 0
@@ -21,6 +16,4 @@ def gen(testId):
     toJson(stats, fPath)
     i += 1
 
-# gen(0)
-stressCmd()
-print(monitor())
+gen(0)

6f33b24cd6e94b675cf3ccf10a1cef387a70672f
Oliver-Warren <aa20575@bristol.ac.uk>
2024-06-19 12:35:07 +0100

    commit

--- nucMonitor/monitoringTool.py
+++ nucMonitor/monitoringTool.py
@@ -1,35 +1,34 @@
 import time
 import json
 import os
-import pduSsh
+from pduSsh import PDU
 
 class NUCmonitor:
 
-  def __init__(self, pduIP, pduUsername, pduPassword, pduOutlet):
-    self.pduIP = pduIP
-    self.pduUsername = pduUsername
-    self.pduPasspord = pduPassword
-    self.pduOutlet = pduOutlet
+  def __init__(self):
 
-  def connectToPdu():
-    self.pdu = PDU(self.pduIP, self.pduUsername, self.pduPassword)
-    power = self.pdu.getOutletPower(self.pduOutlet)
+  def connectToPdu(self, pduIP, pduUsername, pduPassword):
+    self.pdu = PDU(pduIP, pduUsername, pduPassword)
+
+  def readPduPower(self):
+    pduResponse = self.pdu.getOutletPower(self.pduOutlet)
+    return float(pduResponse.split()[3])
+
+  def closePdu(self):
     self.pdu.close()
-    return power
 
+  @staticmethod
   def readCpuEnergy():
     with open("/sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj", "r") as file:
       return float(file.read())
 
+  @staticmethod
   def readCpuTemp():
     with open("/sys/class/thermal/thermal_zone0/temp", "r") as file:
       return {"CPU temp": float(file.read()) / 1000 }
 
-  #
-  # This recieves the average values over 1, 5 and 15 minutes and is not helpful for this case
-  # The driving script only invokes stress-ng for times < 1min so there is little change to these stats
-  # Hence these stats wont make good features for the model
-  #
+  # Not used for features, not instantaneous load
+  @staticmethod
   def readCpuLoad():
     with open("/proc/loadavg") as file:
       stats = file.read()
@@ -37,7 +36,8 @@ class NUCmonitor:
 	      "CPU load (/proc/loadavg)(5m)": float(stats.split()[1]),
 	      "CPU load (/proc/loadavg)(15m)": float(stats.split()[2])}
 
-  # Another way of finding INSTANTANEOUS cpu load is using /proc/stat
+  # Used as a features, instantaneous load
+  @staticmethod
   def readCpuLoadII():
     with open("/proc/stat") as file:
       topLine = file.readline()
@@ -45,6 +45,7 @@ class NUCmonitor:
       out = [int(x) for x in topLine.split()[1:5]]
     return out
 
+  @staticmethod
   def readIfStats():
     ifNames = []
     txBytes = []
@@ -56,12 +57,15 @@ class NUCmonitor:
         txBytes.append(float(line.split()[9]))
     return list(zip(ifNames, rxBytes, txBytes))
 
+  @staticmethod
   def calcCpuPower(e0, e1, t):
     return {"CPU power": ( e1 - e0 ) / ( t * 1000000 ) }
 
+  @staticmethod
   def calcCpuLoadII(l0, l1):
     return {"CPU load (/proc/stat)(1s)": float( sum(l1[0:3]) - sum(l0[0:3]) ) / float( sum(l1) - sum(l0) ) }
 
+  @staticmethod
   def calcIfDatarates(s0, s1, t):
     out = {}
     for i in range(len(s0)):
@@ -69,38 +73,41 @@ class NUCmonitor:
       out.update({s0[i][0]+" TX": (s1[i][2] - s0[i][2]) / t } )
     return out
 
-# all time restrained operations in here to keep reporting interval close to intended
+  # all time restrained operations in here to keep reporting interval close to intended
+  @staticmethod
   def monitor(interval=1.0):
     # first readings
-    cpuEnergy0 = NUCmonitor.readCpuEnergy()
+    # cpuEnergy0 = NUCmonitor.readCpuEnergy()
     ifStats0 = NUCmonitor.readIfStats()
     cpuLoadII0 = NUCmonitor.readCpuLoadII()
     # sleep
     time.sleep(interval)
     # second readings
-    cpuEnergy1 = NUCmonitor.readCpuEnergy()
+    # cpuEnergy1 = NUCmonitor.readCpuEnergy()
     ifStats1 = NUCmonitor.readIfStats()
     cpuLoadII1 = NUCmonitor.readCpuLoadII()
-    cpuTemp = NUCmonitor.readCpuTemp()
+    # cpuTemp = NUCmonitor.readCpuTemp()
     # cpuLoad = NUCmonitor.readCpuLoad()
     # calculate
-    cpuPower = NUCmonitor.calcCpuPower(cpuEnergy0, cpuEnergy1, interval)
+    # cpuPower = NUCmonitor.calcCpuPower(cpuEnergy0, cpuEnergy1, interval)
     ifDatarates = NUCmonitor.calcIfDatarates(ifStats0, ifStats1, interval)
     cpuLoadII = NUCmonitor.calcCpuLoadII(cpuLoadII0, cpuLoadII1)
     # append
     out = {}
-    out.update(cpuTemp)
-    out.update(cpuLoad)
+    # out.update(cpuTemp)
+    ## out.update(cpuLoad)
     out.update(cpuLoadII)
-    out.update(cpuPower)
+    # out.update(cpuPower)
     out.update(ifDatarates)
     return out
 
+  @staticmethod
   def toJson(obj, path="/home/ubuntu/NUCmonitor/nucMonitor/test.json"):
     jsonObj = json.dumps(obj, indent=4)
     with open(path, "w") as file:
       file.write(jsonObj)
 
+  @staticmethod
   def experiment(tests, trial):
     dirPath = "/home/ubuntu/monitoringTool/trial" + str(trial) + "/"
     os.makedirs(os.path.dirname(dirPath), exist_ok=True)
@@ -115,4 +122,7 @@ class NUCmonitor:
 
 # toJson(monitor())
 nucMonitor = NUCmonitor(pduIP="10.68.17.123", pduUsername="apc", pduPassword="apc", pduOutlet="6")
-print(nucMonitor.monitor())
+# print(nucMonitor.monitor(1))
+nucMonitor.connectToPdu()
+print(nucMonitor.readPduPower())
+nucMonitor.closePdu()
--- nucMonitor/pduSsh.py
+++ nucMonitor/pduSsh.py
@@ -9,9 +9,9 @@ class PDU:
     self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     self.client.connect(hostname=pduIP, username=username, password=password, banner_timeout=5)
 
-  def getOutletPower(outlet):
+  def getOutletPower(self, outlet):
     _, stdout, _ = self.client.exec_command("olReading " + str(outlet) + " power")
     return stdout.readline()
 
-  def close():
+  def close(self):
     self.client.close()

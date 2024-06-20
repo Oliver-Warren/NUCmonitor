2f3188cd27be849654d2b378d76fd8bd23791f22
Oliver-Warren <aa20575@bristol.ac.uk>
2024-06-20 13:13:43 +0100

    commit

--- nucMonitor/testMonitor.py
+++ nucMonitor/testMonitor.py
@@ -1,10 +1,11 @@
-from monitoringTool import *
+from monitoringTool import NUCmonitor
 import time
 
 # Periodically reports stats gathered by monitoring tool
 def report():
+  nucMonitor = NUCmonitor("10.68.17.50", "apc", "apc", "6")
   while True:
-    print(monitor())
+    print(nucMonitor.monitor())
     time.sleep(1)
 
 report()

08b5c55cf1e977bc86406b7ee56b89a954c9d09b
Oliver-Warren <aa20575@bristol.ac.uk>
2024-06-24 13:54:24 +0100

    new model

---
+++ model/model.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.metrics import mean_squared_error
+from pathlib import Path
+
+# paths to data
+pathLaptopUbuntu = "/home/ojdwa/NUCmonitor/data/"
+pathLaptopVS = "NUCmonitor\data"
+pathNuc = "/home/ubuntu/NUCmonitor/data"
+
+# model parameters
+n_neighbors = 4
+splitseed = 42
+
+def loadData(path):
+  pathObj = Path(path)
+  return pd.DataFrame([pd.read_json(p, typ="series") for p in pathObj.iterdir()])
+
+def getTargets(data):
+  return data["PDU power"]
+
+def getFeatures(data):
+  return data.drop(["PDU power"], axis=1)
+
+# Get data
+data = loadData(pathLaptopVS)
+print(data.head())
+X = getFeatures(data)
+y = getTargets(data)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=splitseed)
+
+# models
+models = { "Poisson Tree      "  : DecisionTreeRegressor(ccp_alpha=1, criterion=["poisson"], splitter="best"),
+           "Friedman Tree     "  : DecisionTreeRegressor(ccp_alpha=1, criterion="friedman_mse", splitter="best"),
+           "MLP Regressor     "  : MLPRegressor(hidden_layer_sizes=[5, 10 ,5], activation="logistic", learning_rate="adaptive", early_stopping=True),
+           "Kernel Ridge      "  : KernelRidge(alpha=0.8, kernel="linear"),
+           "Linear Regression "  : LinearRegression(),
+           "Random Forest     "  : RandomForestRegressor(),
+           "KNN               "  : KNeighborsRegressor(n_neighbors=n_neighbors, weights="distance") }
+for model in models.keys:
+  print(model)
+  models[model].fit(X_train, y_train)
+  y_pred = models[model].predict(X_test)
+  mse = mean_squared_error(y_test, y_pred)
+  print("MSE:", mse)

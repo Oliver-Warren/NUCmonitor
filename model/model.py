dd63c9212523d0ee9e184f3b86669609430d7a84
Oliver-Warren <aa20575@bristol.ac.uk>
2024-06-25 12:01:00 +0100

    able to save linreg model

--- model/model.py
+++ model/model.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -9,6 +8,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics import mean_squared_error
 from pathlib import Path
+from skl2onnx import to_onnx
+import onnxruntime as rt
 
 # paths to data
 pathLaptopUbuntu = "/home/ojdwa/NUCmonitor/data/"
@@ -29,27 +30,44 @@ def getTargets(data):
 def getFeatures(data):
   return data.drop(["PDU power", "lo RX", "lo TX", "wlp2s0 RX", "wlp2s0 TX"], axis=1)
   
+# get features (X) and targets (y) from file system
+def getData(path):
+  data = loadData(path)
+  X = getFeatures(data)
+  y = getTargets(data)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=splitseed)
+  return X, y, X_train, X_test, y_train, y_test
 
-# Get data
-data = loadData(pathLaptopUbuntu)
-X = getFeatures(data)
-print(X.head())
-y = getTargets(data)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=splitseed)
+# try some different models
+def tryModels(X_train, X_test, y_train, y_test):
+  models = { "Friedman Tree     "  : DecisionTreeRegressor(ccp_alpha=1, criterion="friedman_mse", splitter="best"),
+             "MLP Regressor     "  : MLPRegressor(hidden_layer_sizes=[100, 50], activation="logistic", learning_rate="adaptive", early_stopping=True),
+             "Kernel Ridge      "  : KernelRidge(alpha=0.8),
+             "Linear Regression "  : LinearRegression(),
+             "Random Forest     "  : RandomForestRegressor(),
+             "KNN               "  : KNeighborsRegressor(n_neighbors=n_neighbors, weights="distance") }
+  for name, model in models.items():
+    print(name)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    if mse < 1:
+      print("Predictions:", y_pred[0:5])
+      print("Actuals    :", list(y_test[0:5]))
+    print("MSE:", mse, "\n")
 
-# models
-models = { "Friedman Tree     "  : DecisionTreeRegressor(ccp_alpha=1, criterion="friedman_mse", splitter="best"),
-           "MLP Regressor     "  : MLPRegressor(hidden_layer_sizes=[100, 50], activation="logistic", learning_rate="adaptive", early_stopping=True),
-           "Kernel Ridge      "  : KernelRidge(alpha=0.8),
-           "Linear Regression "  : LinearRegression(),
-           "Random Forest     "  : RandomForestRegressor(),
-           "KNN               "  : KNeighborsRegressor(n_neighbors=n_neighbors, weights="distance") }
-for name, model in models.items():
-  print(name)
-  model.fit(X_train, y_train)
-  y_pred = model.predict(X_test)
-  mse = mean_squared_error(y_test, y_pred)
-  if mse < 1:
-    print("Predictions:", y_pred[0:5])
-    print("Actuals    :", list(y_test[0:5]))
-  print("MSE:", mse, "\n")
+# Linear Regression is the simplest model and shows the most promise
+# This method trains a Linear Regression model and stores it as an .onnx serialisation
+# The model is trained on the ENTIRE dataset
+def makeLinReg(X, y):
+  model = LinearRegression().fit(X, y)
+  onx = to_onnx(model, X[:1])
+  with open("linReg.onnx", "wb") as file:
+    file.write(onx.SerializeToString())
+
+
+# script
+X, y, _, _, _, _ = getData(pathLaptopUbuntu)
+makeLinReg(X, y)
+print("saved model")
+
